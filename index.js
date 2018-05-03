1314593f25ae01741dee4da21b9a54d437c0cf3b
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 18:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ function moveDodgerLeft() {
       DODGER.style.left = `${left - 1}px`
       console.log(DODGER.style.left)
       if (left > moveTo && left > 0) {
-        console.log(left, moveTo)
+        // console.log(left, moveTo)
         window.requestAnimationFrame(step)
       }
     }
