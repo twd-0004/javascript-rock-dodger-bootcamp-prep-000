48a8e5f3c7446b2f4ec9b2f06ef2ffc33bf54a50
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 18:10:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -110,16 +110,7 @@ function createRock(x) {
 function endGame() {
 }
 
-function moveDodger(e) {
-  // implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
-  document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function moveDodger(e) {
     if (e.which === LEFT_ARROW) {
       moveDodgerLeft();
     }
