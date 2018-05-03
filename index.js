77c0a0d9d62e2ccd17d34059e147e1a6598bef2f
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 18:28:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
   if (e.which === LEFT_ARROW) {
-    moveDodgerLeft();
+    requestAnimationFrame(moveDodgerLeft);
   }
   if (e.which === RIGHT_ARROW) {
     moveDodgerRight();
