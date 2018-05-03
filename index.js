287d9097a2de5fa267e1f55a1e88b0824207662b
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 18:25:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -133,17 +133,13 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-  document.addEventListener('keydown', function(e) {
-    if (e.which === LEFT_ARROW) {
-      var leftNumbers = DODGER.style.left.replace('px', '')
-      var left = parseInt(leftNumbers, 10)
-      console.log(leftNumbers)
-      console.log(left)
-      if (left > 0) {
-        DODGER.style.left = `${left - 4}px`
-      }
-    }
-  })
+  var leftNumbers = DODGER.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  console.log(leftNumbers)
+  console.log(left)
+  if (left > 0) {
+    DODGER.style.left = `${left - 4}px`
+  }
 }
 
 function moveDodgerRight() {
